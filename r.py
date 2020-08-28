bb8cf7dffe8e15a431f46a652ba8cc06097550bf
Kai Jie Hu <kaijie.hu@gmail.com>
2020-08-28 14:33:43 +0800

    add counts

--- r.py
+++ r.py
@@ -1,13 +1,16 @@
 import random 
 r = random.randint(1,50)
-
+count = 0
 while True:
+  count = count + 1	
   x = input ('請猜猜看1到50的整數')
   x = int(x)
   if x == r :
-   print ('你猜對了')
-   break
+      print ('你猜對了')
+      print('這是你猜的第', count , '次')
+      break
   elif x > r :
-      print ('比答案大')
+      print ('比答案大耶')
   elif x < r:
       print('比答案小')	
+  print('這是你猜的第', count , '次')
